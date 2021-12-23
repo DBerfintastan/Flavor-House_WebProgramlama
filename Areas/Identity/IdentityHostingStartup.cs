a8755a686a69967051fe34e1f205df68ab0fe5a5
Deniz Berfin Taştan <deniz.tastan@ogr.sakarya.edu.tr>
2021-12-23 18:24:54 +0300

    Login and register pages added

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FlavorHouse.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FlavorHouse.Areas.Identity.IdentityHostingStartup))]
+namespace FlavorHouse.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
