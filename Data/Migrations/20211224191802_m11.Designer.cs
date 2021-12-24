508f3fabc7f1bf75e9c11bb589c36cbb1567b9dd
mustafa melih tüfekcioğlu <mustafa.tufekcioglu@ogr.sakarya.edu.tr>
2021-12-24 23:11:52 +0300

    empty

--- Data/Migrations/20211224191802_m11.Designer.cs
+++ Data/Migrations/20211224191802_m11.Designer.cs
@@ -4,14 +4,16 @@ using FlavorHouse.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FlavorHouse.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211224191802_m11")]
+    partial class m11
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
