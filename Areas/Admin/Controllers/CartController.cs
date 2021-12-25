32d6ff66afb0bbbe829847a61a90ad8404751bf4
mustafa melih tüfekcioğlu <mustafa.tufekcioglu@ogr.sakarya.edu.tr>
2021-12-25 21:16:58 +0300

    general edit

---
+++ Areas/Admin/Controllers/CartController.cs
@@ -0,0 +1,27 @@
+﻿using FlavorHouse.Data;
+using FlavorHouse.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlavorHouse.Areas.Admin.Controllers
+{
+    public class CartController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+        public CartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            var d = _context.carts.ToList<Cart>();
+            return View();
+        }
+    }
+}
---
+++ Data/Migrations/20211225105952_1006.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlavorHouse.Data.Migrations
+{
+    public partial class _1006 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
