f960522a893ca07f5fccedd689ddec6ce89dcd2a
mustafa melih tüfekcioğlu <mustafa.tufekcioglu@ogr.sakarya.edu.tr>
2021-12-26 00:21:05 +0300

    added contact functions

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FlavorHouse.Data;
+using FlavorHouse.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,27 @@ namespace FlavorHouse.Controllers
 {
     public class ContactController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+        public ContactController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            _context.contacts.Add(contact);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
