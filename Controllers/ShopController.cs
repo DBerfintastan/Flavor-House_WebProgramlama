8fa2eb2fff42a333614d813f44f94095b7e60eb9
Deniz Berfin Taştan <deniz.tastan@ogr.sakarya.edu.tr>
2021-12-26 21:21:44 +0300

    Final

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -1,5 +1,6 @@
 ﻿using FlavorHouse.Data;
 using FlavorHouse.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,13 +11,13 @@ namespace FlavorHouse.Controllers
 {
     public class ShopController : Controller
     {
-
+        
         private readonly ApplicationDbContext _context;
         public ShopController(ApplicationDbContext context)
         {
             _context = context;
         }
-
+        [Authorize]
         public IActionResult Index()
         {
             var data = _context.desserts.OrderByDescending(x => x.DessertName).Take(_context.desserts.Count()).ToList();
